ec318d958437edc5d634e95b5a26b1e1fde98d66
endaniel1 <enriq_1997@hotmail.com>
2020-07-30 12:23:43 -0400

    Añadiendo un pequeño proyecto web sencillo

---
+++ ProyectoWeb/ProyectoWebApp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def home(request):
+	
+	return render(request, "ProyectoWebApp/home.html")
+
+def services(request):
+	
+	return render(request, "ProyectoWebApp/services.html")
+
+def shop(request):
+	
+	return render(request, "ProyectoWebApp/shop.html") 
+
+def blog(request):
+	
+	return render(request, "ProyectoWebApp/blog.html")
+
+def contact(request):
+	
+	return render(request, "ProyectoWebApp/contact.html")
