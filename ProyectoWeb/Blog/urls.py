12a3d5f17c4d75fb99fd06fa3970b245195edb01
endaniel1 <enriq_1997@hotmail.com>
2020-10-22 09:01:09 -0400

    Agregando filtro de post por categorias

--- ProyectoWeb/Blog/urls.py
+++ ProyectoWeb/Blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name="Blog"),
+    path('category/<int:category_id>/', views.category, name="Category"),
 ]
--- ProyectoWeb/Blog/views.py
+++ ProyectoWeb/Blog/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post,Category
 # Create your views here.
 
 def blog(request):
 
 	posts = Post.objects.all()
 	return render(request, "blog/blog.html",{
+		"posts": posts
+	})
+
+def category(request, category_id):
+
+	category = Category.objects.get(id = category_id)
+	posts = Post.objects.filter(categories = category)
+	return render(request, "blog/category.html",{
+		"category": category,
 		"posts": posts
 	})
