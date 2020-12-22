17e9a5f18cd68eb72682dcd157403512942173ce
endaniel1 <enriq_1997@hotmail.com>
2020-12-22 18:55:03 -0400

    subiendo estilos al formaulario de contact

--- ProyectoWeb/contacto/forms.py
+++ ProyectoWeb/contacto/forms.py
@@ -5,5 +5,5 @@ class ContactForm(forms.Form):
 
 	name = forms.CharField(label='Nombre', required=True, max_length="150")
 	email = forms.EmailField(label='Correo', required=True)
-	content = forms.CharField(label='Contenido')
+	content = forms.CharField(label='Contenido', widget=forms.Textarea)
 	
